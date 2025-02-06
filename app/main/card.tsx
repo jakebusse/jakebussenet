2acb3e5d0fa32505d8cc17fe98a803b1dd70a5aa
Jake Busse <78458447+jakebusse@users.noreply.github.com>
2025-02-05 19:07:31 -0600

    fix scrolling

--- app/main/card.tsx
+++ app/main/card.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import "./globals.css";
 
 type CardProps = {
   id: string;
@@ -11,7 +10,7 @@ type CardProps = {
 export default function Card({ id, bg, fg, children }: CardProps) {
   return (
     <div
-      className={`card w-full h-full overflow-y-scroll overscroll-contain sticky top-0 rounded-xl p-6 ${bg} ${fg} ${
+      className={`card w-full h-full overflow-y-scroll rounded-xl p-6 ${bg} ${fg} ${
         id !== "hero" ? "mt-[15vh]" : ""
       } `}
       id={id.toString()}
--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,10 @@ import {
   FaPhoneAlt,
 } from "react-icons/fa";
 import { FaBluesky } from "react-icons/fa6";
-import Card from "./card";
+import Card from "./main/card";
 import GradientHeader from "./gradient-header";
 import "./globals.css";
-import "./main.css";
+import "./main/main.css";
 
 export default function Home() {
   const [navOpen, setNavOpen] = useState(false);
@@ -22,49 +22,6 @@ export default function Home() {
   const [foreground, setForeground] = useState("text-gray-100");
   const [specialCursor, setSpecialCursor] = useState(false);
   const [time, setTime] = useState(new Date());
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const cards = [
-    {
-      image: "https://source.unsplash.com/random",
-      title: "Card 1",
-      description: "Info which directs to the other page.",
-    },
-    {
-      image: "https://source.unsplash.com/random",
-      title: "Card 2",
-      description: "Info which directs to the other page.",
-    },
-    {
-      image: "https://source.unsplash.com/random",
-      title: "Card 3",
-      description: "Info which directs to the other page.",
-    },
-    {
-      image: "https://source.unsplash.com/random",
-      title: "Card 4",
-      description: "Info which directs to the other page.",
-    },
-    {
-      image: "https://source.unsplash.com/random",
-      title: "Card 5",
-      description: "Info which directs to the other page.",
-    },
-  ];
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % cards.length);
-  };
-
-  const handlePrev = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + cards.length) % cards.length
-    );
-  };
-  if (currentIndex > cards.length - 3) {
-    console.log(currentIndex);
-    setCurrentIndex(0);
-    console.log("hi");
-  }
 
   useEffect(() => {
     const interval = setInterval(() => setTime(new Date()), 1000);
@@ -98,28 +55,141 @@ export default function Home() {
       id: "linkedin",
       icon: <FaLinkedinIn />,
       url: "https://linkedin.com/in/jakebusse",
+      nav: true,
     },
     {
       id: "instagram",
       icon: <FaInstagram />,
       url: "https://instagram.com/jakerbusse",
+      nav: false,
     },
     {
       id: "bluesky",
       icon: <FaBluesky />,
       url: "https://bsky.app/profile/jakerbusse.bsky.social",
+      nav: false,
     },
     {
       id: "email",
       icon: <FaEnvelope />,
       url: "mailto:jakerbusse@outlook.com",
+      nav: true,
     },
     {
       id: "phone",
       icon: <FaPhoneAlt />,
       url: "tel:6516051932",
+      nav: true,
     },
   ];
+
+  const cards = [
+    {
+      id: "hero",
+      key: 1,
+      bg: "transparent",
+      fg: "white",
+      content: (
+        <div className="w-full h-full flex flex-col items-center justify-center gap-4">
+          <Image
+            src={avatarHover ? "/main/avatar_wink.png" : "/main/avatar.png"}
+            alt={"Jake Busse avatar"}
+            width={100}
+            height={100}
+            onMouseEnter={() => setAvatarHover(true)}
+            onMouseLeave={() => {
+              setAvatarHover(false);
+              setSpecialCursor(true);
+            }}
+            className="transition-all duration-500 ease-in-out z-30 w-auto h-auto"
+            priority
+          />
+          <h2 className="text-4xl text-white font-medium text-center">
+            Good{" "}
+            {formattedTime < 12
+              ? "Morning"
+              : formattedTime === 12
+              ? "Day"
+              : formattedTime > 12 && formattedTime < 5
+              ? "Afternoon"
+              : "Evening"}
+            , I'm Jake
+          </h2>
+          <span className="text-2xl text-white text-center">
+            Welcome to my humble corner of the web.
+          </span>
+        </div>
+      ),
+    },
+    {
+      id: "about",
+      key: 2,
+      content: (
+        <div className="w-full h-full flex flex-row flex-no-wrap gap-6 justify-between">
+          <div className="w-full md:w-2/3 space-y-4">
+            <GradientHeader>About Me</GradientHeader>
+            <p>Hey, I’m Jake.</p>
+            <p>
+              I’m a firm believer that curiosity and caffeine fuel the best
+              ideas. Whether I’m troubleshooting a stubborn piece of technology
+              or diving into a new topic just for the thrill of learning, I’m
+              always looking for ways to make things work better, faster, and
+              smarter.
+            </p>
+            <p>
+              Beyond the world of IT, I’m an avid thinker, road trip planner,
+              and proud cat parent. My four feline companions keep me grounded
+              (and mildly entertained), proving that life is best balanced
+              between structured problem-solving and spontaneous chaos.
+            </p>
+            <p>
+              I thrive on understanding how things work—whether it’s technology,
+              systems, or people. My approach to life is a mix of pragmatism,
+              humor, and just enough overthinking to keep things interesting.
+              While I spend plenty of time in the tech world, I believe the best
+              solutions come from looking beyond the screen and connecting with
+              people.
+            </p>
+            <p>
+              When I’m not deep in a project or wrangling my ever-growing to-do
+              list, you’ll probably find me exploring new places or planning my
+              next adventure. Always happy to chat—especially if it involves
+              tech, cats, or something interesting to figure out!
+            </p>
+          </div>
+          <div className="hidden md:block bg-[url(/main/headshot.png)] bg-center bg-contain bg-no-repeat md:bg-cover w-full min-h-full md:w-1/3 rounded-lg">
+            &nbsp;
+          </div>
+        </div>
+      ),
+    },
+    {
+      id: "experience",
+      key: 3,
+      content: <GradientHeader>Experience</GradientHeader>,
+    },
+    {
+      id: "education",
+      key: 4,
+      content: <GradientHeader>Education</GradientHeader>,
+    },
+    {
+      id: "portfolio",
+      key: 5,
+      content: <GradientHeader>Portfolio</GradientHeader>,
+    },
+    {
+      id: "certifications",
+      key: 6,
+      content: <GradientHeader>Certifications</GradientHeader>,
+    },
+    {
+      id: "contact",
+      key: 7,
+      content: <GradientHeader>Contact</GradientHeader>,
+    },
+  ];
+
   return (
     <div
       className={`w-screen h-screen gradient overflow-hidden flex flex-col gap-0 fixed top-0 bottom-0 left-0 right-0 ${
@@ -176,103 +246,31 @@ export default function Home() {
               : "hidden md:block md:text-white"
           }`}
         >
-          {socialIcons.map((socialIcon) => (
-            <a href={socialIcon.url} target="_blank" key={socialIcon.id}>
-              <li className="rounded-full md:border border-white p-3 transition duration-500 ease-in-out md:hover:text-primary md:hover:bg-white">
-                {socialIcon.icon}
-              </li>
-            </a>
-          ))}
+          {socialIcons
+            .filter((socialIcon) => socialIcon.nav)
+            .map((socialIcon) => (
+              <a href={socialIcon.url} target="_blank" key={socialIcon.id}>
+                <li className="rounded-full md:border border-white p-3 transition duration-500 ease-in-out md:hover:text-primary md:hover:bg-white">
+                  {socialIcon.icon}
+                </li>
+              </a>
+            ))}
         </ul>
       </nav>
       <div
         id="container"
         className="fixed top-[100px] right-0 bottom-0 left-0 overflow-y-scroll p-8 pt-0 gap-32 select-none focus:outline-none"
       >
-        <Card id="hero" bg="transparent" fg="white">
-          <div className="w-full h-full flex flex-col items-center justify-center gap-4">
-            <Image
-              src={avatarHover ? "/main/avatar_wink.png" : "/main/avatar.png"}
-              alt={"Jake Busse avatar"}
-              width={100}
-              height={100}
-              onMouseEnter={() => setAvatarHover(true)}
-              onMouseLeave={() => {
-                setAvatarHover(false);
-                setSpecialCursor(true);
-              }}
-              className="transition-all duration-500 ease-in-out z-30 w-auto h-auto"
-              priority
-            />
-            <h2 className="text-4xl text-white font-medium text-center">
-              Good{" "}
-              {formattedTime < 12
-                ? "Morning"
-                : formattedTime === 12
-                ? "Day"
-                : formattedTime > 12 && formattedTime < 5
-                ? "Afternoon"
-                : "Evening"}
-              , I'm Jake
-            </h2>
-            <span className="text-2xl text-white text-center">
-              Welcome to my humble corner of the web.
-            </span>
-          </div>
-        </Card>
-        <Card id="about" bg={background} fg={foreground}>
-          <div className="w-full h-full flex flex-row flex-no-wrap gap-6 justify-between">
-            <div className="w-full md:w-2/3 space-y-4">
-              <GradientHeader>About Me</GradientHeader>
-              <p>Hey, I’m Jake.</p>
-              <p>
-                I’m a firm believer that curiosity and caffeine fuel the best
-                ideas. Whether I’m troubleshooting a stubborn piece of
-                technology or diving into a new topic just for the thrill of
-                learning, I’m always looking for ways to make things work
-                better, faster, and smarter.
-              </p>
-              <p>
-                Beyond the world of IT, I’m an avid thinker, road trip planner,
-                and proud cat parent. My four feline companions keep me grounded
-                (and mildly entertained), proving that life is best balanced
-                between structured problem-solving and spontaneous chaos.
-              </p>
-              <p>
-                I thrive on understanding how things work—whether it’s
-                technology, systems, or people. My approach to life is a mix of
-                pragmatism, humor, and just enough overthinking to keep things
-                interesting. While I spend plenty of time in the tech world, I
-                believe the best solutions come from looking beyond the screen
-                and connecting with people.
-              </p>
-              <p>
-                When I’m not deep in a project or wrangling my ever-growing
-                to-do list, you’ll probably find me exploring new places or
-                planning my next adventure. Always happy to chat—especially if
-                it involves tech, cats, or something interesting to figure out!
-              </p>
-            </div>
-            <div className="hidden md:block bg-[url(/main/headshot.png)] bg-center bg-contain bg-no-repeat md:bg-cover w-full min-h-full md:w-1/3 rounded-lg">
-              &nbsp;
-            </div>
-          </div>
-        </Card>
-        <Card id="experience" bg={background} fg={foreground}>
-          <GradientHeader>Experience</GradientHeader>
-        </Card>
-        <Card id="education" bg={background} fg={foreground}>
-          <GradientHeader>Education</GradientHeader>
-        </Card>
-        <Card id="portfolio" bg={background} fg={foreground}>
-          <GradientHeader>Portfolio</GradientHeader>
-        </Card>
-        <Card id="certifications" bg={background} fg={foreground}>
-          <GradientHeader>Certifications</GradientHeader>
-        </Card>
-        <Card id="contact" bg={background} fg={foreground}>
-          <GradientHeader>Contact</GradientHeader>
-        </Card>
+        {cards.map((card) => (
+          <Card
+            key={card.key}
+            id={card.id}
+            bg={card.bg ? card.bg : background}
+            fg={card.fg ? card.fg : foreground}
+          >
+            {card.content}
+          </Card>
+        ))}
       </div>
     </div>
   );
